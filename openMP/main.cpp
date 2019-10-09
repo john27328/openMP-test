bd04cb1bbeb958a143709d0a886593c521946107
Raptev Ivan <john27328@gmail.com>
2019-10-09 13:56:43 +0300

    test2

--- openMP/main.cpp
+++ openMP/main.cpp
@@ -1,27 +1,19 @@
-#include <stdio.h>
+#include <iostream>
 #include <omp.h>
 
-#define N 10000
+#define N 10000000000000000
 
 int main(int argc, char *argv[])
 {
-  double a[N], b[N], c[N];
-  int i;
+  long long i;
   omp_set_dynamic(0);      // запретить библиотеке openmp менять число потоков во время исполнения
   omp_set_num_threads(10); // установить число потоков в 10
 
-  // инициализируем массивы
-  for (i = 0; i < N; i++)
-  {
-      a[i] = i * 1.0;
-      b[i] = i * 2.0;
-  }
-
   // вычисляем сумму массивов
-#pragma omp parallel for shared(a, b, c) private(i)
-   for (i = 0; i < N; i++)
-     c[i] = a[i] + b[i];
-
-  printf ("%f\n", c[10]);
+#pragma omp parallel for
+  for (i = 0; i < N; i++){
+      double c = i + 1./i;
+  }
+    std::cout << N << std::endl;
   return 0;
 }
